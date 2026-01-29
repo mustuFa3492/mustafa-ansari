3fcca43b8a5333971fb1af0ce4a9cb25b276c258
Gulammustufa Ansari <gmansari0064@gmail.com>
2026-01-29 15:59:35 +0530

    Add files via upload

--- src/MainSection.tsx
+++ src/MainSection.tsx
@@ -1,14 +1,11 @@
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Environment } from "@react-three/drei";
-import Avatar from "./Avatar";
 
 function MainSection() {
   return (
     <Canvas camera={{ position: [0, 1.5, 4], fov: 50 }}>
       <ambientLight intensity={0.8} />
       <directionalLight position={[5, 5, 5]} intensity={1.2} />
-
-      <Avatar />
 
       <OrbitControls
         enableZoom={false}
