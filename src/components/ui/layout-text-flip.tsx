7a9d3cc33296ef2dbbc328c63e78378a0c6c5e09
mustuFa3492 <gmansari0064@gmail.com>
2026-01-30 11:48:10 +0530

    Fixed import error

--- src/components/ui/layout-text-flip.tsx
+++ src/components/ui/layout-text-flip.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils.ts";
 
 export const LayoutTextFlip = ({
   text = "Build Amazing",
